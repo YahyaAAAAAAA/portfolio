926eda1dc0ac169684156f92f86fcfe2bec83c48
Yahya <44685017+YahyaAmarneh@users.noreply.github.com>
2025-07-28 00:41:14 +0300

    Built web
    Branch: main
    Commit: fa9254a1c519fa7183eb8ef0d28d8bc8f548ed3e (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3680313396"
+    serviceWorkerVersion: "326101028"
   }
 });
